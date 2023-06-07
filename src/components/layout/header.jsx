24d9cb01081925b31fbfb2b013a4a4600ed848fc
KyawLwinSoe27 <soek38902@gmail.com>
2023-06-07 08:31:41 +0630

    project structure

---
+++ src/components/layout/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../assets/styles/layouts/header.css';
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className='text'>
+            Hello World!
+        </h1>
+    </div>
+  )
+}
+
+export default Header
